515bf302e16762b85c9cb63cddb5ef3f942561ab
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2024-06-11 20:02:42 +0300

    Changed callback dispatcher

--- src/core/callback.rs
+++ src/core/callback.rs
@@ -30,42 +30,10 @@ pub(crate) struct CallbackContainer {
 
 pub(crate) trait CallbackDispatcher: Send + Sync {
     type Item: CallbackTyped;
+    type Output;
 
-    fn storage(&self) -> &SingleDispatcher<Self::Item>;
-
-    fn register(&self) -> UnboundedReceiver<Self::Item> {
-        let storage = &self.storage().inner;
-        let mut guard = storage.lock();
-        let (sender, receiver) = futures::channel::mpsc::unbounded();
-
-        if guard.replace(sender).is_some() {
-            tracing::warn!(
-                "Callback {} have already registered, old request will be cancelled",
-                std::any::type_name::<Self>()
-            )
-        }
-
-        receiver
-    }
-
-    fn proceed(&self, value: Self::Item) {
-        let storage = &self.storage().inner;
-        let guard = storage.lock();
-
-        if let Some(sender) = &*guard {
-            match sender.unbounded_send(value) {
-                Ok(_) => {
-                    tracing::debug!("Sent callback: {}", std::any::type_name::<Self>())
-                }
-                Err(_) => {
-                    tracing::error!(
-                        "Callback {} have received, but receiver is broken",
-                        std::any::type_name::<Self>()
-                    )
-                }
-            }
-        }
-    }
+    fn register(&self) -> Output;
+    fn proceed(&self, value: Self::Item);
 }
 
 #[derive(Debug)]
@@ -83,9 +51,40 @@ impl<T: CallbackTyped> Default for SingleDispatcher<T> {
 
 impl<T: CallbackTyped> CallbackDispatcher for SingleDispatcher<T> {
     type Item = T;
+    type Output = UnboundedReceiver<Self::Item>;
 
-    fn storage(&self) -> &SingleDispatcher<Self::Item> {
-        self
+    fn register(&self) -> Self::Output {
+        let storage = &self.inner;
+        let mut guard = storage.lock();
+        let (sender, receiver) = futures::channel::mpsc::unbounded();
+
+        if guard.replace(sender).is_some() {
+            tracing::warn!(
+                "Callback {} have already registered, old request will be cancelled",
+                std::any::type_name::<Self>()
+            )
+        }
+
+        receiver
+    }
+
+    fn proceed(&self, value: Self::Item) {
+        let storage = &self.inner;
+        let guard = storage.lock();
+
+        if let Some(sender) = &*guard {
+            match sender.unbounded_send(value) {
+                Ok(_) => {
+                    tracing::debug!("Sent callback: {}", std::any::type_name::<Self>())
+                }
+                Err(_) => {
+                    tracing::error!(
+                        "Callback {} have received, but receiver is broken",
+                        std::any::type_name::<Self>()
+                    )
+                }
+            }
+        }
     }
 }
 
